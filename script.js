06a7c4d61d980115bda79a57fc25b2a5bfb62c1a
steliviere <32293736+steliviere@users.noreply.github.com>
2018-10-29 21:22:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function lefter()
 	if(current_img>1)
 	{
 		current_img-=1;
+		current_imgLoad();
 	}
 }
 function righter()
@@ -11,11 +12,22 @@ function righter()
 	if(current_img<8)
 	{
 		current_img+=1;
+		current_imgLoad();
 	}
 }
 function linker(n)
 {
 	current_img=n;
+	current_imgLoad();
+}
+function current_imgLoad()
+{
+	var viewer=document.getElementById("current_img"));
+	viewer.src="img/"+imgData[current_img].title+".png";
+	viewer.title=imgData[current_img].alt;
+	viewer.alt=imgData[current_img].alt;
+	var desc=document.getElementById("description"));
+	desc.innerHTML=imgData[current_img].description;
 }
 window.onload=function()
 {
@@ -25,5 +37,7 @@ window.onload=function()
 	{
 		var down=document.getElementById("under_img"+(i+1));
 		down.src="img/"+imgData[i].title+".png";
+		down.title=imgData[i].alt;
+		down.alt=imgData[i].alt;
 	}
 };
